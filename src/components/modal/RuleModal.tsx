5b41a7737848bee41274b130537aa16c6ae3a696
서지유 <jihoseo2008@gmail.com>
2025-07-16 09:16:38 +0900

    feat :: 규칙 추가

--- src/components/modal/RuleModal.tsx
+++ src/components/modal/RuleModal.tsx
@@ -25,10 +25,19 @@ export const RuleModal = ({ isOpen, onClose }: IProps) => {
       <Wrapper>
         <Text variant="TitleSmall">토론 규칙</Text>
 
-        <Text variant="LabelMedium" color={`${theme.colors.sub.normal[20]}`}>
-          토론에 사용할 수 있는 글자 수는 총 2000자로,
-          <br /> 토론 전 과정에서 공유되어 소모됩니다
-        </Text>
+        <TextWrapper>
+          <Text variant="LabelMedium" color={`${theme.colors.gray[600]}`}>
+            토론은 입론(60초), 반론·변론·최종 변론(각 180초) 순으로 진행됩니다.
+          </Text>
+
+          <Text variant="LabelMedium" color={`${theme.colors.gray[600]}`}>
+            총 글자 수 2000자를 토론 전 과정에서 공유하여 사용합니다.
+          </Text>
+
+          <Text variant="LabelMedium" color={`${theme.colors.gray[600]}`}>
+            상호 존중과 논리적 근거 제시를 부탁드립니다.
+          </Text>
+        </TextWrapper>
 
         <ButtonWrapper>
           <Button onClick={onClose} variant="blue" size="large">
@@ -57,4 +66,10 @@ const LeftTime = styled.div`
   position: absolute;
   top: 24px;
   left: 24px;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
