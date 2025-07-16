3e447af047fe9e8817fd40a4929c3b7c67ff7057
서지유 <jihoseo2008@gmail.com>
2025-07-16 09:09:39 +0900

    feat :: 토론매칭 아이템 컴포넌트

---
+++ src/components/debate/DebateItem.tsx
@@ -0,0 +1,57 @@
+import styled from "@emotion/styled";
+import { Button } from "../common/Button";
+
+interface IProps {
+  title: string;
+  tags: string[];
+  stance: "찬성" | "반대";
+}
+
+export const DebateItem = ({ title, tags, stance }: IProps) => {
+  return (
+    <DebateItemWrapper>
+      <div>
+        <Question>{title}</Question>
+        <TagList>
+          {tags.map(tag => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </TagList>
+      </div>
+
+      <ButtonWrapper>
+        <Button>{stance}</Button>
+      </ButtonWrapper>
+    </DebateItemWrapper>
+  );
+};
+
+const DebateItemWrapper = styled.div`
+  padding: 16px 0;
+  border-top: ${({ theme }) => `1px solid ${theme.colors.gray[300]}`};
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Question = styled.h3`
+  margin: 0 0 8px 0;
+  font-size: 16px;
+`;
+
+const TagList = styled.div`
+  display: flex;
+  gap: 8px;
+  margin-bottom: 8px;
+`;
+
+const Tag = styled.div`
+  background-color: ${({ theme }) => theme.colors.blue[400]};
+  color: ${({ theme }) => theme.colors.sub.normal[10]};
+  padding: 6px 12px;
+  border-radius: 20px;
+  font-size: 14px;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 80px;
+`;
